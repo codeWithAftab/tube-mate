aacda1a2ed059656c3b49c04cbf51c0c23253afc
aftab <aftabahmad41442@gmail.com>
2022-07-12 10:49:15 +0530

    initial commit

--- tubemate/urls.py
+++ tubemate/urls.py
@@ -1,15 +1,12 @@
 import imp
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , include,re_path
 from . import settings
 from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("downloader.urls")),
-    
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
